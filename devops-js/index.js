fa51259061e7c771344e664a1610952e4d0d0b43
Emmerson Matheus Rodrigues de Souza <emmersonsouza99@proton.me>
2025-11-01 13:54:59 -0300

    feature github actions

---
+++ devops-js/index.js
@@ -0,0 +1,3 @@
+console.log('Hello, World!');
+console.log('This is the index.js file.');
+console.log('learning CI;CD with github actions');
---
+++ devops-js/test.js
@@ -0,0 +1,5 @@
+console.log('Starting TEST file execution.');
+setTimeout(() => {
+    console.log('waiting 3 secs');
+}, 3000);
+console.log('TEST file execution completed.');
